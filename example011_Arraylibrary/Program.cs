21acb4eab8f30dce4be2a903fc6efcabc6b365de
EKATERINA <evlesnykh@gmail.com>
2023-01-18 23:00:25 +1000

    add example011 Arraylibrary

---
+++ example011_Arraylibrary/Program.cs
@@ -0,0 +1,40 @@
+﻿//Пример№1
+// void FillArray(int[] collection);
+// {
+//   int length = collection.Length;
+//   int index = 0;
+//   while (index < length)
+//   {
+//     collection[index] = new Random().Next(1, 10);
+//     index = index + 1; //index++
+//   }
+// }
+//Пример№2
+// void FillArray(int[] collection)
+// void PrintArray(int[] col)
+// int[] array = new int[10];
+// {
+//   int count = col.Length;
+//   int position = 0;
+//   while (position < count) ;
+//   {
+//     Console.WriteLine(col[position]);
+//     position++;
+//   }
+// }
+
+// int[] array = new int[10];
+
+// FillArray(array);
+// PrintArray(array);
+
+// //Пример№3:
+void FillArray(int[] collection);
+
+void PrintArray(int[] col);
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+
+
