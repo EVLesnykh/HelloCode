5b4b77dfa54efd6e3de20f4f73548deeedb5efe0
EKATERINA <evlesnykh@gmail.com>
2023-01-22 21:19:08 +1000

    add correct example011

--- example011_Arraylibrary/Program.cs
+++ example011_Arraylibrary/Program.cs
@@ -1,32 +1,32 @@
 ﻿//Пример№1
-// void FillArray(int[] collection);
-// {
-//   int length = collection.Length;
-//   int index = 0;
-//   while (index < length)
-//   {
-//     collection[index] = new Random().Next(1, 10);
-//     index = index + 1; //index++
-//   }
-// }
+ void FillArray(int[] collection);
+ {
+   int length = collection.Length;
+  int index = 0;
+   while (index < length)
+{
+collection[index] = new Random().Next(1, 10);
+index = index + 1; //index++
+}
+ }
 //Пример№2
-// void FillArray(int[] collection)
-// void PrintArray(int[] col)
-// int[] array = new int[10];
-// {
-//   int count = col.Length;
-//   int position = 0;
-//   while (position < count) ;
-//   {
-//     Console.WriteLine(col[position]);
-//     position++;
-//   }
-// }
+ void FillArray(int[] collection)
+ void PrintArray(int[] col)
+int[] array = new int[10];
+ {
+   int count = col.Length;
+  int position = 0;
+while (position < count) ;
+  {
+    Console.WriteLine(col[position]);
+     position++;
+  }
+}
 
-// int[] array = new int[10];
+int[] array = new int[10];
 
-// FillArray(array);
-// PrintArray(array);
+ FillArray(array);
+ PrintArray(array);
 
 //Пример№3:
 void FillArray(int[] collection);
