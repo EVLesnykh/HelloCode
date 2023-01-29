95104d8e9f39f2537457983103ac204dee25d641
EKATERINA <evlesnykh@gmail.com>
2023-01-29 20:40:49 +1000

    add example 013 Recurcion

---
+++ example013_Recursion/Program.cs
@@ -0,0 +1,220 @@
+﻿//пример 1:
+// string[,] table = new string[2, 5];
+// // String.Empty
+// // table [0,0] table [0,1] table [0,2] table [0,4] 
+// // table [1,0] table [1,1] table [1,2] table [1,4]
+// table[1, 2] = "слово";
+// for (int rows = 0; rows < 2; rows++)
+// {
+//   for (int columns = 0; columns < 5; columns++)
+//   {
+//     Console.WriteLine($"-{table[rows, columns]}-");
+//   }
+// }
+
+// пример 2.1:
+// int[,] matrix = new int[3, 4];
+// for (int i = 0; i < 3; i++)
+// {
+//   for (int j = 0; j < 4; j++)
+//   {
+//     Console.Write($"{matrix[i, j]} ");
+//   }
+//  Console.WriteLine();
+// }
+
+//пример 2.2:
+// int[,] matrix = new int[3, 4];
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//   for (int j = 0; j < matrix.GetLength(1); j++)
+//   {
+//     Console.Write($"{matrix[i, j]} ");
+//   }
+//   Console.WriteLine();
+// }
+
+//Заполнение матрицы случайными числами:
+// int[,] matrix = new int[3, 4];
+
+// void PrintArray(int[,] matr)
+// {
+//   for (int i = 0; i < matr.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < matr.GetLength(1); j++)
+//     {
+//       Console.Write($"{matr[i, j]} ");
+//     }
+//     Console.WriteLine();
+//   }
+// }
+// PrintArray(matrix);
+
+//Дополнительный метод:
+// int[,] matrix = new int[3, 4];
+
+// void PrintArray(int[,] matr)
+// {
+//   for (int i = 0; i < matr.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < matr.GetLength(1); j++)
+//     {
+//       Console.Write($"{matr[i, j]} ");
+//     }
+//     Console.WriteLine();
+//   }
+// }
+
+// void FillArray(int[,] matr)
+// {
+//   for (int i = 0; i < matr.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < matr.GetLength(1); j++)
+//     {
+//       matr[i, j] = new Random().Next(1, 10);//[1; 10)
+//     }
+//   }
+// }
+// PrintArray(matrix);
+// FillArray(matrix); 
+// Console.WriteLine();
+// PrintArray(matrix);
+
+//Задача про пиксели:
+int[,] pic = new int[,]
+{
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+{0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+
+void PrintImage(int[,] image)
+{
+  for (int i = 0; i < image.GetLength(0); i++)
+  {
+    for (int j = 0; j < image.GetLength(1); j++)
+    {
+      if (image[i, j] == 0) Console.Write($" ");
+      else Console.Write($"+");
+    }
+    Console.WriteLine();
+  }
+}
+//Добавим метод, который будет закрашивать картинку
+
+// void FillImage(int row, int col)
+// {
+//   if (pic[row, col] == 0)
+//   {
+//     pic[row, col] = 1;
+//     FillImage(row - 1, col); 
+//     FillImage(row, col - 1); 
+//     FillImage(row + 1, col); 
+//     FillImage(row, col + 1);
+//   }
+// }
+// PrintImage(pic);
+// FillImage(13, 13);
+// PrintImage(pic);
+
+//Что же есть рекурсия?
+//Факторил N! 
+// 5! = 5 * 4 * 3 * 2 * 1
+//пример 1
+// int Factorial(int n)
+// {
+//   // 1! = 1
+//   // 0! = 1
+//   if (n == 1) return 1;
+//   else return n * Factorial(n - 1);
+// }
+// Console.WriteLine(Factorial(3)); // 1 * 2 * 3 = 6
+
+//пример 2. Если надо найти факторил большого числа 40!
+// int Factorial(int n)
+// {
+//   // 1! = 1
+//   // 0! = 1
+//   if (n == 1) return 1;
+//   else return n * Factorial(n - 1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//   Console.WriteLine(Factorial(i));
+// }
+
+//переполнение типа, надо проверить до какого факториала можем считать
+// int Factorial(int n)
+// {
+// // 1! = 1
+// // 0! = 1
+// if(n == 1) return 1;
+// else return n * Factorial(n-1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+// Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+//заменим int на double
+// double Factorial(int n)
+// {
+// // 1! = 1
+// // 0! = 1
+// if(n == 1) return 1;
+// else return n * Factorial(n-1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+// Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+//Число Фибоначчи
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+//решение:
+
+// int Fibonacci(int n)
+// {
+//   if (n == 1 || n == 2) return 1;
+//   else return Fibonacci(n - 1) + Fibonacci(n - 2);
+// }
+// for (int i = 1; i < 10; i++)
+// {
+//   Console.WriteLine(Fibonacci(i));
+// }
+
+//перезапишем
+//посчитаем первые 50 чисел
+
+double Fibonacci(int n)
+{
+  if (n == 1 || n == 2) return 1;
+  else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for (int i = 1; i < 50; i++)
+{
+  Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
